d98a4fcae97af813585afe2b26c68d554b2c60cd
AmanGupta <amandsvv4006@gmail.com>
2024-08-23 13:15:33 +0530

    PRime function added

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-function myfunc (){
-    var x = 5;
-    var y = 10;
-    var z = x + y;
-    console.log(z);
+function findPrime(num) {
+    let count = 0;
+    for (let i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            count++;
+        }
+    }
+    return count;
 }
-myfunc();
+
+let num = 12;
+
+if(findPrime(num) == 2){
+    console.log(`${num} is a prime number`);
+}
+else{
+    console.log(`${num} is not a prime number`);
+}
